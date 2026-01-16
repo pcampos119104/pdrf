392fa66512635f151fdd8f9a5983ff6e4e6dd357
Pedro Campos <pcampos119104@gmail.com>
2026-01-16 10:06:09 -0300

    first commit

---
+++ src/pdrf/crews/pdr/crew.py
@@ -0,0 +1,228 @@
+import os
+
+from crewai import LLM
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+# from crewai_tools import DallETool
+
+
+@CrewBase
+class ProcessadorDeReceitasCrewaiCrew:
+    """ProcessadorDeReceitasCrewai crew"""
+
+    @agent
+    def conversor_de_medidas_culinarias(self) -> Agent:
+        return Agent(
+            config=self.agents_config["conversor_de_medidas_culinarias"],
+            tools=[],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            max_execution_time=None,
+            llm=LLM(
+                model="ollama/qwen2.5:14b",
+                temperature=0.7,
+            ),
+        )
+
+    # @agent
+    # def gerador_de_imagem_culinaria(self) -> Agent:
+    #     return Agent(
+    #         config=self.agents_config["gerador_de_imagem_culinaria"],
+    #         tools=[DallETool()],
+    #         reasoning=False,
+    #         max_reasoning_attempts=None,
+    #         inject_date=True,
+    #         allow_delegation=False,
+    #         max_iter=25,
+    #         max_rpm=None,
+    #         max_execution_time=None,
+    #         llm=LLM(
+    #             model="ollama/qwen2.5:14b",
+    #             temperature=0.7,
+    #         ),
+    #     )
+
+    @agent
+    def compilador_de_receitas(self) -> Agent:
+        return Agent(
+            config=self.agents_config["compilador_de_receitas"],
+            tools=[],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            max_execution_time=None,
+            llm=LLM(
+                model="ollama/qwen2.5:14b",
+                temperature=0.7,
+            ),
+        )
+
+    @agent
+    def extrator_de_titulo(self) -> Agent:
+        return Agent(
+            config=self.agents_config["extrator_de_titulo"],
+            tools=[],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            max_execution_time=None,
+            llm=LLM(
+                model="ollama/qwen2.5:14b",
+                temperature=0.7,
+            ),
+        )
+
+    @agent
+    def extrator_de_descricao(self) -> Agent:
+        return Agent(
+            config=self.agents_config["extrator_de_descricao"],
+            tools=[],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            max_execution_time=None,
+            llm=LLM(
+                model="ollama/qwen2.5:14b",
+                temperature=0.7,
+            ),
+        )
+
+    @agent
+    def extrator_de_ingredientes(self) -> Agent:
+        return Agent(
+            config=self.agents_config["extrator_de_ingredientes"],
+            tools=[],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            max_execution_time=None,
+            llm=LLM(
+                model="ollama/qwen2.5:14b",
+                temperature=0.7,
+            ),
+        )
+
+    @agent
+    def extrator_de_modo_de_preparo(self) -> Agent:
+        return Agent(
+            config=self.agents_config["extrator_de_modo_de_preparo"],
+            tools=[],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            max_execution_time=None,
+            llm=LLM(
+                model="ollama/qwen2.5:14b",
+                temperature=0.7,
+            ),
+        )
+
+    @agent
+    def gerador_de_tags(self) -> Agent:
+        return Agent(
+            config=self.agents_config["gerador_de_tags"],
+            tools=[],
+            reasoning=False,
+            max_reasoning_attempts=None,
+            inject_date=True,
+            allow_delegation=False,
+            max_iter=25,
+            max_rpm=None,
+            max_execution_time=None,
+            llm=LLM(
+                model="ollama/qwen2.5:14b",
+                temperature=0.7,
+            ),
+        )
+
+    @task
+    def extrair_ingredientes_da_receita(self) -> Task:
+        return Task(
+            config=self.tasks_config["extrair_ingredientes_da_receita"],
+            markdown=False,
+        )
+
+    @task
+    def extrair_titulo_da_receita(self) -> Task:
+        return Task(
+            config=self.tasks_config["extrair_titulo_da_receita"],
+            markdown=False,
+        )
+
+    @task
+    def extrair_descricao_da_receita(self) -> Task:
+        return Task(
+            config=self.tasks_config["extrair_descricao_da_receita"],
+            markdown=False,
+        )
+
+    @task
+    def extrair_modo_de_preparo_da_receita(self) -> Task:
+        return Task(
+            config=self.tasks_config["extrair_modo_de_preparo_da_receita"],
+            markdown=False,
+        )
+
+    @task
+    def gerar_tags_da_receita(self) -> Task:
+        return Task(
+            config=self.tasks_config["gerar_tags_da_receita"],
+            markdown=False,
+        )
+
+    @task
+    def converter_medidas_dos_ingredientes(self) -> Task:
+        return Task(
+            config=self.tasks_config["converter_medidas_dos_ingredientes"],
+            markdown=False,
+        )
+
+    # @task
+    # def gerar_imagem_da_receita(self) -> Task:
+    #     return Task(
+    #         config=self.tasks_config["gerar_imagem_da_receita"],
+    #         markdown=False,
+    #     )
+
+    @task
+    def compilar_receita_final(self) -> Task:
+        return Task(
+            config=self.tasks_config["compilar_receita_final"],
+            markdown=False,
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the ProcessadorDeReceitasCrewai crew"""
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            chat_llm=LLM(model="ollama/qwen2.5:14b"),
+        )
+
+    def _load_response_format(self, name):
+        with open(os.path.join(self.base_directory, "config", f"{name}.json")) as f:
+            json_schema = json.loads(f.read())
+
+        return SchemaConverter.build(json_schema)
---
+++ src/pdrf/crews/pdr/main.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+import sys
+from pdr.crew import ProcessadorDeReceitasCrewaiCrew
+
+# This main file is intended to be a way for your to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'lista_ingredientes': 'sample_value',
+        'titulo_receita': 'sample_value',
+        'descricao_receita': 'sample_value',
+        'titulo': 'sample_value',
+        'descricao': 'sample_value',
+        'ingredientes_convertidos': 'sample_value',
+        'modo_preparo': 'sample_value',
+        'tags': 'sample_value',
+        'receita_texto': '''Bolo de Cenoura com Farinha de Aveia
+
+15 Minutos Preparo
+
+    25 Porções
+
+Lista de ingredientes
+
+    2 cenouras descascadas e cortadas em rodelas
+    ½ xícara (chá) de óleo
+    3 ovos
+    2 xícaras (chá) de açúcar
+    2 ½ xícaras (chá) de Farinha de Aveia Yoki
+    1 colher (sopa) de Fermento em Pó
+    ½ xícara (chá) de nozes picadas
+
+Preparação
+
+    Bata no liquidificador a cenoura, o óleo e os ovos.
+    Retire a mistura do liquidificador e coloque em uma vasilha.
+    Acrescente o açúcar, a farinha de aveia, o fermento e as nozes picadas. Mexa delicadamente a receita de bolo de cenoura simples.
+    Unte e enfarinhe uma assadeira retangular média.
+    Coloque a massa do bolo de cenoura na assadeira e leve ao forno preaquecido a 200°C por 45 minutos ou até dourar.
+    Agora você já sabe como fazer bolo de cenoura simples!'''
+    }
+    ProcessadorDeReceitasCrewaiCrew().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        'lista_ingredientes': 'sample_value',
+        'titulo_receita': 'sample_value',
+        'descricao_receita': 'sample_value',
+        'titulo': 'sample_value',
+        'descricao': 'sample_value',
+        'ingredientes_convertidos': 'sample_value',
+        'modo_preparo': 'sample_value',
+        'tags': 'sample_value',
+        'receita_texto': 'sample_value'
+    }
+    try:
+        ProcessadorDeReceitasCrewaiCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        ProcessadorDeReceitasCrewaiCrew().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        'lista_ingredientes': 'sample_value',
+        'titulo_receita': 'sample_value',
+        'descricao_receita': 'sample_value',
+        'titulo': 'sample_value',
+        'descricao': 'sample_value',
+        'ingredientes_convertidos': 'sample_value',
+        'modo_preparo': 'sample_value',
+        'tags': 'sample_value',
+        'receita_texto': 'sample_value'
+    }
+    try:
+        ProcessadorDeReceitasCrewaiCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: main.py <command> [<args>]")
+        sys.exit(1)
+
+    command = sys.argv[1]
+    if command == "run":
+        run()
+    elif command == "train":
+        train()
+    elif command == "replay":
+        replay()
+    elif command == "test":
+        test()
+    else:
+        print(f"Unknown command: {command}")
+        sys.exit(1)
